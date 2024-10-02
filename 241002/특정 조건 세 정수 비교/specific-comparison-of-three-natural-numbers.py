8ceca2360922a77d32acf8235b7eeb0962bcec5e
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-02 17:46:49 +0900

    Title: 특정 조건 세 정수 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241002/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 241002/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,10 +1,10 @@
 a, b, c = map(int, input().split())
 
-if b>c:
+if b>=c:
     if a==c:
         print(1, end=" ")
     else: print(0, end=" ")
-if b<c:
+if b=<c:
     if a==b:
         print(1, end=" ")
     else: print(0, end=" ")
