2eb435589da4c657695af6f60bb3f34e6e67a377
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-04 12:48:52 +0900

    Title: 369 게임, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241004/369 게임/369-game.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i%3==0:
+        print(0, end=" ")
+    elif i>10 and i//10%3==0:
+        print(0, end=" ")
+    else:
+        print(i, end=" ")
