3ad2bc7ec99f88d54c0e64a955b54aee35411f81
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-04 12:52:44 +0900

    Title: 369 게임, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/369 게임/369-game.py
+++ 241004/369 게임/369-game.py
@@ -5,7 +5,7 @@ for i in range(1, n+1):
         print(0, end=" ")
     elif i>10 and i//10%3==0:
         print(0, end=" ")
-    elif i%10%3==0:
+    elif i%10!=0 and i%10%3==0:
         print(0, end=" ")
     else:
         print(i, end=" ")
