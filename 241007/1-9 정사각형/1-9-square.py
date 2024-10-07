9f74693ea276a95ba178f5942ee268352575e70a
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-07 23:50:29 +0900

    Title: 1-9 정사각형, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/1-9 정사각형/1-9-square.py
+++ 241007/1-9 정사각형/1-9-square.py
@@ -3,8 +3,8 @@ cnt= 1
 
 for i in range(n):
     for j in range(n):
-        print(cnt%10, end="")
+        print(cnt, end="")
         cnt+=1
-        if cnt%10==0:
-            cnt+=1
+        if cnt==10:
+            cnt=1
     print()
