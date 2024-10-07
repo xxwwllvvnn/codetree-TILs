da7766539ecbdddde3c0e99bc24f7a9b0980943c
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-07 23:08:43 +0900

    Title: 체크 다이아몬드, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    for k in range(i+1):
+        print("* ", end="")
+    print()
+for i in range(n-1):
+    for j in range(i+1):
+        print(" ", end="")
+    for k in range(n-i-1):
+        print("* ", end="")
+    print()
