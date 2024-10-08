b14ee5886b2702ea0ceed177d15e9c183b020e97
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-08 14:57:10 +0900

    Title: 정상적인 구구단, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241008/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print("%d * %d = %d" % (i, j, i*j), end="")
+        if j!=n:
+            print(", ", end="")
+    print()
