c64148637f971c28e191e7d279303b5b1be86683
xxwwllvvnn <168844475+xxwwllvvnn@users.noreply.github.com>
2024-10-17 14:59:12 +0900

    Title: 배열의 평균, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/배열의 평균/ave-of-array.py
+++ 241017/배열의 평균/ave-of-array.py
@@ -4,11 +4,11 @@ for _ in range(2):
     arr.append(list(map(int, input().split())))
 
 for i in range(2):
-    print("%.1f" % sum(arr[i])/4, end=" ")
+    print("%.1f" % (sum(arr[i])/4), end=" ")
 print()
 
 for i in range(4):
-    print("%.1f" % sum(arr[_][i] for _ in range(2))/2, end=" ")
+    print("%.1f" % (sum(arr[_][i] for _ in range(2))/2), end=" ")
 print()
 
-print("%.1f" % sum(arr[i][j] for i in range(2) for j in range(4))/8)
+print("%.1f" % (sum(arr[i][j] for i in range(2) for j in range(4))/8))
